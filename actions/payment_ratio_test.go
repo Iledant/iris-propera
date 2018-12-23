b5cbf7abf65c5792d7ef7baab3ceb718aaef0347
Iledant <christophe.saintillan@gmail.com>
2018-12-23 17:19:30 +0100

    Payment Ratio refactored

--- actions/payment_ratio_test.go
+++ actions/payment_ratio_test.go
@@ -2,6 +2,7 @@ package actions
 
 import (
 	"net/http"
+	"strings"
 	"testing"
 
 	"github.com/iris-contrib/httpexpect"
@@ -26,14 +27,23 @@ func getRatiosTest(e *httpexpect.Expect, t *testing.T) {
 		{Token: testCtx.User.Token, Status: http.StatusOK,
 			BodyContains: []string{"PaymentRatio"}, ArraySize: 26},
 	}
-	for _, tc := range testCases {
+	for i, tc := range testCases {
 		response := e.GET("/api/payment_ratios").WithHeader("Authorization", "Bearer "+tc.Token).Expect()
+		content := string(response.Content)
 		for _, s := range tc.BodyContains {
-			response.Body().Contains(s)
+			if !strings.Contains(content, s) {
+				t.Errorf("\nGetRatios[%d] :\n  attendu ->\"%s\"\n  reçu <-\"%s\"", i, s, content)
+			}
 		}
-		response.Status(tc.Status)
-		if tc.Status == http.StatusOK {
-			response.JSON().Object().Value("PaymentRatio").Array().Length().Equal(tc.ArraySize)
+		statusCode := response.Raw().StatusCode
+		if statusCode != tc.Status {
+			t.Errorf("\nGetRatios[%d],statut :  attendu ->%v  reçu <-%v", i, tc.Status, statusCode)
+		}
+		if tc.ArraySize > 0 {
+			count := strings.Count(content, `"id"`)
+			if count != tc.ArraySize {
+				t.Errorf("\nGetRatios[%d] :\n  nombre attendu -> %d\n  nombre reçu <-%d", i, tc.ArraySize, count)
+			}
 		}
 	}
 }
@@ -43,20 +53,29 @@ func getPtRatiosTest(e *httpexpect.Expect, t *testing.T) {
 	testCases := []testCase{
 		{Token: "fake", ID: "0", Status: http.StatusInternalServerError,
 			BodyContains: []string{"Token invalide"}},
-		{Token: testCtx.User.Token, Status: http.StatusBadRequest,
-			ID: "0", BodyContains: []string{"Liste des ratios : chronique introuvable"}},
+		{Token: testCtx.User.Token, Status: http.StatusOK,
+			ID: "0", BodyContains: []string{`"PaymentRatio":null`}},
 		{Token: testCtx.User.Token, Status: http.StatusOK,
 			ID: "5", BodyContains: []string{"PaymentRatio"}, ArraySize: 8},
 	}
-	for _, tc := range testCases {
+	for i, tc := range testCases {
 		response := e.GET("/api/payment_types/"+tc.ID+"/payment_ratios").
 			WithHeader("Authorization", "Bearer "+tc.Token).Expect()
+		content := string(response.Content)
 		for _, s := range tc.BodyContains {
-			response.Body().Contains(s)
+			if !strings.Contains(content, s) {
+				t.Errorf("\nGetPtRatios[%d] :\n  attendu ->\"%s\"\n  reçu <-\"%s\"", i, s, content)
+			}
 		}
-		response.Status(tc.Status)
-		if tc.Status == http.StatusOK {
-			response.JSON().Object().Value("PaymentRatio").Array().Length().Equal(tc.ArraySize)
+		statusCode := response.Raw().StatusCode
+		if statusCode != tc.Status {
+			t.Errorf("\nGetPtRatios[%d],statut :  attendu ->%v  reçu <-%v", i, tc.Status, statusCode)
+		}
+		if tc.ArraySize > 0 {
+			count := strings.Count(content, `"id"`)
+			if count != tc.ArraySize {
+				t.Errorf("\nGetPtRatios[%d] :\n  nombre attendu -> %d\n  nombre reçu <-%d", i, tc.ArraySize, count)
+			}
 		}
 	}
 }
@@ -66,23 +85,35 @@ func setPtRatiosTest(e *httpexpect.Expect, t *testing.T) {
 	testCases := []testCase{
 		{Token: testCtx.User.Token, ID: "0", Status: http.StatusUnauthorized,
 			BodyContains: []string{"Droits administrateur requis"}},
-		{Token: testCtx.Admin.Token, ID: "0", Status: http.StatusBadRequest,
-			BodyContains: []string{"Ratios : chronique introuvable"}},
+		{Token: testCtx.Admin.Token, ID: "0", Status: http.StatusInternalServerError,
+			Sent: []byte(`{"PaymentRatio":[{"ratio":0.05,"index":0},
+		{"ratio":0.1,"index":1},{"ratio":0.15,"index":2},{"ratio":0.25,"index":3},
+		{"ratio":0.45,"index":4}]}`),
+			BodyContains: []string{"Ratios d'une chronique, requête : pq"}},
 		{Token: testCtx.Admin.Token, ID: "5", Status: http.StatusOK,
 			Sent: []byte(`{"PaymentRatio":[{"ratio":0.05,"index":0},
 			{"ratio":0.1,"index":1},{"ratio":0.15,"index":2},{"ratio":0.25,"index":3},
 			{"ratio":0.45,"index":4}]}`),
 			BodyContains: []string{"PaymentRatio", `"ratio":0.05,"index":0`}, ArraySize: 5},
 	}
-	for _, tc := range testCases {
+	for i, tc := range testCases {
 		response := e.POST("/api/payment_types/"+tc.ID+"/payment_ratios").
 			WithHeader("Authorization", "Bearer "+tc.Token).WithBytes(tc.Sent).Expect()
+		content := string(response.Content)
 		for _, s := range tc.BodyContains {
-			response.Body().Contains(s)
+			if !strings.Contains(content, s) {
+				t.Errorf("\nSetPtRatios[%d] :\n  attendu ->\"%s\"\n  reçu <-\"%s\"", i, s, content)
+			}
 		}
-		response.Status(tc.Status)
-		if tc.Status == http.StatusOK {
-			response.JSON().Object().Value("PaymentRatio").Array().Length().Equal(tc.ArraySize)
+		statusCode := response.Raw().StatusCode
+		if statusCode != tc.Status {
+			t.Errorf("\nSetPtRatios[%d],statut :  attendu ->%v  reçu <-%v", i, tc.Status, statusCode)
+		}
+		if tc.ArraySize > 0 {
+			count := strings.Count(content, `"id"`)
+			if count != tc.ArraySize {
+				t.Errorf("\nSetPtRatios[%d] :\n  nombre attendu -> %d\n  nombre reçu <-%d", i, tc.ArraySize, count)
+			}
 		}
 	}
 }
@@ -92,51 +123,64 @@ func deletePtRatiosTest(e *httpexpect.Expect, t *testing.T) {
 	testCases := []testCase{
 		{Token: testCtx.User.Token, ID: "0", Status: http.StatusUnauthorized,
 			BodyContains: []string{"Droits administrateur requis"}},
-		{Token: testCtx.Admin.Token, ID: "0", Status: http.StatusBadRequest,
-			BodyContains: []string{"Suppression de ratios : introuvable"}},
+		{Token: testCtx.Admin.Token, ID: "0", Status: http.StatusInternalServerError,
+			BodyContains: []string{"Suppression des ratios d'une chronique, requête : Ratios de paiement introuvables"}},
 		{Token: testCtx.Admin.Token, ID: "5", Status: http.StatusOK,
 			BodyContains: []string{"Ratios supprimés"}},
 	}
-	for _, tc := range testCases {
+	for i, tc := range testCases {
 		response := e.DELETE("/api/payment_types/"+tc.ID+"/payment_ratios").
 			WithHeader("Authorization", "Bearer "+tc.Token).Expect()
+		content := string(response.Content)
 		for _, s := range tc.BodyContains {
-			response.Body().Contains(s)
+			if !strings.Contains(content, s) {
+				t.Errorf("\nDeletePtRatios[%d] :\n  attendu ->\"%s\"\n  reçu <-\"%s\"", i, s, content)
+			}
 		}
-		response.Status(tc.Status)
+		statusCode := response.Raw().StatusCode
+		if statusCode != tc.Status {
+			t.Errorf("\nDeletePtRatios[%d],statut :  attendu ->%v  reçu <-%v", i, tc.Status, statusCode)
+		}
 		if tc.Status == http.StatusOK {
 			resp := e.GET("/api/payment_types/"+tc.ID+"/payment_ratios").
 				WithHeader("Authorization", "Bearer "+tc.Token).Expect()
-			resp.JSON().Object().Value("PaymentRatio").Array().Length().Equal(0)
+			content = string(resp.Content)
+			expected := `"PaymentRatio":null`
+			if !strings.Contains(content, expected) {
+				t.Errorf("\nDeletePtRatios[%d] :\n  attendu ->\"%s\"\n  reçu <-\"%s\"", i, expected, content)
+			}
 		}
 	}
 }
 
 // getYearRatiosTest check route is protected and ratios correctly calculated
 func getYearRatiosTest(e *httpexpect.Expect, t *testing.T) {
-	testCases := []struct {
-		Token        string
-		Year         string
-		Status       int
-		BodyContains []string
-		ArraySize    int
-	}{
+	testCases := []testCase{
 		{Token: "fake", Status: http.StatusInternalServerError,
 			BodyContains: []string{"Token invalide"}},
 		{Token: testCtx.User.Token, Status: http.StatusBadRequest,
-			BodyContains: []string{"Ratios annuels : année manquante"}, ArraySize: 8},
-		{Token: testCtx.User.Token, Status: http.StatusOK, Year: "2011",
+			BodyContains: []string{"Ratios annuels : année manquante"}},
+		{Token: testCtx.User.Token, Status: http.StatusOK, Param: "2011",
 			BodyContains: []string{"Ratios", `"ratio":0.108592`}, ArraySize: 8},
 	}
-	for _, tc := range testCases {
+	for i, tc := range testCases {
 		response := e.GET("/api/payment_ratios/year").WithHeader("Authorization", "Bearer "+tc.Token).
-			WithQuery("Year", tc.Year).Expect()
+			WithQuery("Year", tc.Param).Expect()
+		content := string(response.Content)
 		for _, s := range tc.BodyContains {
-			response.Body().Contains(s)
+			if !strings.Contains(content, s) {
+				t.Errorf("\nGetYearRatios[%d] :\n  attendu ->\"%s\"\n  reçu <-\"%s\"", i, s, content)
+			}
 		}
-		response.Status(tc.Status)
-		if tc.Status == http.StatusOK {
-			response.JSON().Object().Value("Ratios").Array().Length().Equal(tc.ArraySize)
+		statusCode := response.Raw().StatusCode
+		if statusCode != tc.Status {
+			t.Errorf("\nGetYearRatios[%d],statut :  attendu ->%v  reçu <-%v", i, tc.Status, statusCode)
+		}
+		if tc.ArraySize > 0 {
+			count := strings.Count(content, `"index"`)
+			if count != tc.ArraySize {
+				t.Errorf("\nGetYearRatios[%d] :\n  nombre attendu -> %d\n  nombre reçu <-%d", i, tc.ArraySize, count)
+			}
 		}
 	}
 }
--- models/payment_ratio.go
+++ models/payment_ratio.go
@@ -1,14 +1,145 @@
 package models
 
+import (
+	"database/sql"
+	"errors"
+	"strconv"
+	"strings"
+)
+
 // PaymentRatio model
 type PaymentRatio struct {
-	ID            int       `json:"id" gorm:"column:id"`
+	ID            int64     `json:"id" gorm:"column:id"`
 	PaymentTypeID NullInt64 `json:"payment_types_id" gorm:"column:payment_types_id"`
 	Ratio         float64   `json:"ratio" gorm:"column:ratio"`
-	Index         int       `json:"index" gorm:"column:index"`
+	Index         int64     `json:"index" gorm:"column:index"`
 }
 
-// TableName ensures table name for payment_ratios
-func (u PaymentRatio) TableName() string {
-	return "payment_ratios"
+// PaymentRatios embeddes an array of PaymentRatio for json export.
+type PaymentRatios struct {
+	PaymentRatios []PaymentRatio `json:"PaymentRatio"`
+}
+
+// PaymentRatioLine embeddes a line sent to set the payments ratios of a payment type.
+type PaymentRatioLine struct {
+	Ratio float64 `json:"ratio"`
+	Index int64   `json:"index"`
+}
+
+// PaymentRatiosBatch embeddes an array of payment ratios lines
+type PaymentRatiosBatch struct {
+	PaymentRatios []PaymentRatioLine `json:"PaymentRatio"`
+}
+
+// YearRatio is used to scan and encode an year ratio
+type YearRatio struct {
+	Index int64   `json:"index"`
+	Ratio float64 `json:"ratio"`
+}
+
+// YearRatios embeddes an array of YearRatio for json export.
+type YearRatios struct {
+	YearRatios []YearRatio `json:"Ratios"`
+}
+
+// GetAll fetches all payment ratios from database.
+func (p *PaymentRatios) GetAll(db *sql.DB) (err error) {
+	rows, err := db.Query(`SELECT id,payment_types_id,ratio,index FROM payment_ratios`)
+	if err != nil {
+		return err
+	}
+	var r PaymentRatio
+	defer rows.Close()
+	for rows.Next() {
+		if err = rows.Scan(&r.ID, &r.PaymentTypeID, &r.Ratio, &r.Index); err != nil {
+			return err
+		}
+		p.PaymentRatios = append(p.PaymentRatios, r)
+	}
+	err = rows.Err()
+	return err
+}
+
+// GetPaymentTypeAll fetches all payment ratios linked to a payment type from database.
+func (p *PaymentRatios) GetPaymentTypeAll(paymentTypeID int64, db *sql.DB) (err error) {
+	rows, err := db.Query(`SELECT id,payment_types_id,ratio,index 
+	FROM payment_ratios WHERE payment_types_id = $1`, paymentTypeID)
+	if err != nil {
+		return err
+	}
+	var r PaymentRatio
+	defer rows.Close()
+	for rows.Next() {
+		if err = rows.Scan(&r.ID, &r.PaymentTypeID, &r.Ratio, &r.Index); err != nil {
+			return err
+		}
+		p.PaymentRatios = append(p.PaymentRatios, r)
+	}
+	err = rows.Err()
+	return err
+}
+
+// DeleteRatios removes a payment ratios linked to a payment type from database.
+func (p *PaymentType) DeleteRatios(db *sql.DB) (err error) {
+	res, err := db.Exec("DELETE FROM payment_ratios WHERE payment_types_id = $1", p.ID)
+	if err != nil {
+		return err
+	}
+	count, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if count == 0 {
+		return errors.New("Ratios de paiement introuvables")
+	}
+	return nil
+}
+
+// Save a batch of payment ratios to the database
+func (p *PaymentRatiosBatch) Save(paymentTypeID int64, db *sql.DB) (err error) {
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	if _, err = tx.Exec(`DELETE FROM payment_ratios WHERE payment_types_id = $1`, paymentTypeID); err != nil {
+		tx.Rollback()
+		return err
+	}
+	sPtID := toSQL(paymentTypeID)
+	var values []string
+	var value string
+	for _, pr := range p.PaymentRatios {
+		value = "(" + sPtID + ", " + toSQL(pr.Ratio) + "," + toSQL(pr.Index) + ")"
+		values = append(values, value)
+	}
+	qry := "INSERT into payment_ratios (payment_types_id, ratio, index) VALUES " + strings.Join(values, ",")
+	if _, err = tx.Exec(qry); err != nil {
+		tx.Rollback()
+		return err
+	}
+	err = tx.Commit()
+	return err
+}
+
+// GetAll fetches the ratios of payment transformation of commitment of a given year.
+func (y *YearRatios) GetAll(year int64, db *sql.DB) (err error) {
+	sy := strconv.FormatInt(year, 10)
+	rows, err := db.Query(`WITH yc AS (SELECT f.id FROM financial_commitment f WHERE f.coriolis_year = $1),
+	total AS (SELECT sum(f.value) as total FROM financial_commitment f WHERE f.id IN (SELECT id FROM yc))
+	SELECT extract(YEAR from p.date) - $2 AS index, SUM(p.value/total.total)::double precision AS ratio
+	FROM payment p, total
+	WHERE p.financial_commitment_id IN (SELECT id FROM yc) GROUP BY index ORDER BY index`, sy, year)
+	if err != nil {
+		return err
+	}
+	var r YearRatio
+	defer rows.Close()
+	for rows.Next() {
+		if err = rows.Scan(&r.Index, &r.Ratio); err != nil {
+			return err
+		}
+		y.YearRatios = append(y.YearRatios, r)
+	}
+	err = rows.Err()
+	return err
 }
