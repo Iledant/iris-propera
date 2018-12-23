a05f9ce52985fd27972dab07ee75c5c11499e947
Iledant <christophe.saintillan@gmail.com>
2018-12-23 18:10:39 +0100

    Prev commitment refactored

--- actions/prev_commitment.go
+++ actions/prev_commitment.go
@@ -8,77 +8,20 @@ import (
 	"github.com/kataras/iris"
 )
 
-// prevCommitmentReq is used to decode one line of previsional commitment batch
-type prevCommitmentReq struct {
-	Number     string             `json:"number"`
-	Year       int64              `json:"year"`
-	Value      int64              `json:"value"`
-	TotalValue models.NullInt64   `json:"total_value"`
-	StateRatio models.NullFloat64 `json:"state_ratio"`
-}
-
-// batchPrevCommitment is used to decode batch payload
-type batchPrevCommitment struct {
-	Pcs []prevCommitmentReq `json:"PrevCommitment"`
-}
-
 // BatchPrevCommitments handles the post request to upload an array of previsional commitments
 func BatchPrevCommitments(ctx iris.Context) {
-	req := batchPrevCommitment{}
+	var req models.PrevCommitmentBatch
 	if err := ctx.ReadJSON(&req); err != nil {
 		ctx.StatusCode(http.StatusInternalServerError)
-		ctx.JSON(jsonError{"Batch prévision d'engagements : erreur décodage " + err.Error()})
+		ctx.JSON(jsonError{"Batch prévision d'engagements : décodage " + err.Error()})
 		return
 	}
-
 	db := ctx.Values().Get("db").(*gorm.DB)
-	tx := db.Begin()
-	if err := tx.Exec("DROP TABLE IF EXISTS temp_prev_commitment").Error; err != nil {
+	if err := req.Save(db.DB()); err != nil {
 		ctx.StatusCode(http.StatusInternalServerError)
-		ctx.JSON(jsonError{"Batch prévision d'engagements, suppression table temporaire : " + err.Error()})
-		tx.Rollback()
+		ctx.JSON(jsonError{"Batch prévision d'engagements : requête " + err.Error()})
 		return
 	}
-	if err := tx.Exec(`CREATE TABLE temp_prev_commitment (number varchar(10), year integer, value bigint,
-	 total_value bigint, state_ratio double precision)`).Error; err != nil {
-		ctx.StatusCode(http.StatusInternalServerError)
-		ctx.JSON(jsonError{"Batch prévision d'engagements, création table temporaire : " + err.Error()})
-		tx.Rollback()
-		return
-	}
-
-	for _, pc := range req.Pcs {
-		if err := tx.Exec(`INSERT INTO temp_prev_commitment (number,year,value,total_value,state_ratio) VALUES(?,?,?,?,?)`,
-			pc.Number, pc.Year, pc.Value, pc.TotalValue, pc.StateRatio).Error; err != nil {
-			ctx.StatusCode(http.StatusInternalServerError)
-			ctx.JSON(jsonError{"Batch prévision d'engagements, insertion table temporaire : " + err.Error()})
-			tx.Rollback()
-			return
-		}
-	}
-	if err := tx.Exec(`UPDATE prev_commitment SET value = t.value, total_value = t.total_value, 
-	state_ratio = t.state_ratio FROM temp_prev_commitment t, physical_op op
-	WHERE t.number=op.number AND prev_commitment.physical_op_id = op.id AND t.year = prev_commitment.year`).Error; err != nil {
-		ctx.StatusCode(http.StatusInternalServerError)
-		ctx.JSON(jsonError{"Batch prévision d'engagements, update : " + err.Error()})
-		tx.Rollback()
-		return
-	}
-	if err := tx.Exec(`INSERT INTO prev_commitment (physical_op_id, year, value, descript, total_value, state_ratio)
-	SELECT op.id, t.year, t.value, NULL, t.total_value, t.state_ratio FROM physical_op op, temp_prev_commitment t
-	WHERE op.number = t.number AND ((op.id, t.year) NOT IN (SELECT physical_op_id, year FROM prev_commitment))`).Error; err != nil {
-		ctx.StatusCode(http.StatusInternalServerError)
-		ctx.JSON(jsonError{"Batch prévision d'engagements, insert : " + err.Error()})
-		tx.Rollback()
-		return
-	}
-	if err := tx.Exec("DROP TABLE IF EXISTS temp_prev_commitment").Error; err != nil {
-		ctx.StatusCode(http.StatusInternalServerError)
-		ctx.JSON(jsonError{"Batch prévision d'engagements, suppression table temporaire : " + err.Error()})
-		tx.Rollback()
-		return
-	}
-	tx.Commit()
 	ctx.StatusCode(http.StatusOK)
 	ctx.JSON(jsonMessage{"Batch prévision d'engagement importé"})
 }
--- models/prev_commitment.go
+++ models/prev_commitment.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"database/sql"
+	"strings"
+)
+
 // PrevCommitment model
 type PrevCommitment struct {
 	ID           int         `json:"id" gorm:"column:id"`
@@ -11,7 +16,68 @@ type PrevCommitment struct {
 	TotalValue   NullInt64   `json:"total_value" gorm:"column:total_value"`
 }
 
-// TableName ensures table name for prev_commitment
-func (u PrevCommitment) TableName() string {
-	return "prev_commitment"
+// PrevCommitments embeddes an array of PrevCommitment.
+type PrevCommitments struct {
+	PrevCommitments []PrevCommitment `json:"PrevCommitment"`
+}
+
+// PrevCommitmentLine is used to decode a line of prevision commiment batch.
+type PrevCommitmentLine struct {
+	Number     string      `json:"number"`
+	Year       int64       `json:"year"`
+	Value      int64       `json:"value"`
+	TotalValue NullInt64   `json:"total_value"`
+	StateRatio NullFloat64 `json:"state_ratio"`
+}
+
+// PrevCommitmentBatch embeddes an array of PrevCommitmentLine.
+type PrevCommitmentBatch struct {
+	PrevCommitments []PrevCommitmentLine `json:"PrevCommitment"`
+}
+
+// Save inserts and updates a batch of prevision commitments into database.
+func (p *PrevCommitmentBatch) Save(db *sql.DB) (err error) {
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	if _, err = tx.Exec("DROP TABLE IF EXISTS temp_prev_commitment"); err != nil {
+		tx.Rollback()
+		return err
+	}
+	if _, err := tx.Exec(`CREATE TABLE temp_prev_commitment (number varchar(10), 
+	year integer, value bigint, total_value bigint, state_ratio double precision)`); err != nil {
+		tx.Rollback()
+		return err
+	}
+	var value string
+	var values []string
+	for _, pc := range p.PrevCommitments {
+		value = "(" + toSQL(pc.Number) + "," + toSQL(pc.Year) + "," + toSQL(pc.Value) +
+			"," + toSQL(pc.TotalValue) + "," + toSQL(pc.StateRatio) + ")"
+		values = append(values, value)
+	}
+	if _, err = tx.Exec(`INSERT INTO temp_prev_commitment (number,year,value,
+		total_value,state_ratio) VALUES ` + strings.Join(values, ",")); err != nil {
+		tx.Rollback()
+		return err
+	}
+	if _, err = tx.Exec(`UPDATE prev_commitment SET value = t.value, total_value = t.total_value, 
+	state_ratio = t.state_ratio FROM temp_prev_commitment t, physical_op op
+	WHERE t.number=op.number AND prev_commitment.physical_op_id = op.id AND t.year = prev_commitment.year`); err != nil {
+		tx.Rollback()
+		return err
+	}
+	if _, err = tx.Exec(`INSERT INTO prev_commitment (physical_op_id, year, value, descript, total_value, state_ratio)
+	SELECT op.id, t.year, t.value, NULL, t.total_value, t.state_ratio FROM physical_op op, temp_prev_commitment t
+	WHERE op.number = t.number AND ((op.id, t.year) NOT IN (SELECT physical_op_id, year FROM prev_commitment))`); err != nil {
+		tx.Rollback()
+		return err
+	}
+	if _, err = tx.Exec("DROP TABLE IF EXISTS temp_prev_commitment"); err != nil {
+		tx.Rollback()
+		return err
+	}
+	err = tx.Commit()
+	return err
 }
