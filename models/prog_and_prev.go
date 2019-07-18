fc38f57a4aa1f48648c5a8d37ecb53cd84882cda
Iledant <christophe.saintillan@gmail.com>
2019-07-18 23:35:24 +0200

    Add preprog to summaries

--- models/prog_and_prev.go
+++ models/prog_and_prev.go
@@ -4,10 +4,13 @@ import "database/sql"
 
 // ProgrammingAndPrevision is used to decode one line of dedicated query.
 type ProgrammingAndPrevision struct {
-	Name         string    `json:"name"`
-	Number       string    `json:"number"`
-	Programmings NullInt64 `json:"programmings"`
-	Prevision    NullInt64 `json:"prevision"`
+	Name            string     `json:"name"`
+	Number          string     `json:"number"`
+	Programmings    NullInt64  `json:"programmings"`
+	PreProgrammings NullInt64  `json:"pre_programmings"`
+	Prevision       NullInt64  `json:"prevision"`
+	CategoryName    NullString `json:"category_name"`
+	ChapterCode     NullInt64  `json:"chapter_code"`
 }
 
 // ProgrammingAndPrevisions embeddes an array of ProgrammingAndPrevision for json export.
@@ -17,12 +20,23 @@ type ProgrammingAndPrevisions struct {
 
 // GetAll fetches programmation and commitments prevision for the given year
 func (p *ProgrammingAndPrevisions) GetAll(year int64, db *sql.DB) (err error) {
-	rows, err := db.Query(`SELECT op.number, op.name, pr.value as programmings, 
-	  pc.value as prevision FROM physical_op op
+	rows, err := db.Query(`SELECT op.number, op.name, pr.value AS programmings, 
+	ppr.value AS pre_programmings, pc.value AS prevision, cat.name AS category_name, 
+	bud.code AS chapter_code FROM physical_op op
+	LEFT OUTER JOIN category cat ON op.category_id = cat.id
+	LEFT OUTER JOIN
+	(SELECT ba.id, bc.code FROM budget_action ba
+		JOIN budget_program bp ON ba.program_id = bp.id
+		JOIN budget_chapter bc ON bp.chapter_id = bc.id ) bud 
+	ON op.budget_action_id = bud.id 
 	LEFT OUTER JOIN
 	(SELECT p.physical_op_id, SUM(value) AS value FROM programmings p, commissions c 
 	WHERE p.commission_id = c.id AND extract(year FROM c.date) = $1 GROUP BY 1) pr
 	ON op.id = pr.physical_op_id
+	LEFT OUTER JOIN
+	(SELECT p.physical_op_id, SUM(value) AS value FROM pre_programmings p, commissions c 
+	WHERE p.commission_id = c.id AND extract(year FROM c.date) = $1 GROUP BY 1) ppr
+	ON op.id = ppr.physical_op_id
 	LEFT OUTER JOIN
 	(SELECT f.physical_op_id, value FROM prev_commitment f WHERE year = $1) pc
 	ON op.id = pc.physical_op_id
@@ -34,7 +48,8 @@ func (p *ProgrammingAndPrevisions) GetAll(year int64, db *sql.DB) (err error) {
 	var r ProgrammingAndPrevision
 	defer rows.Close()
 	for rows.Next() {
-		if err = rows.Scan(&r.Number, &r.Name, &r.Programmings, &r.Prevision); err != nil {
+		if err = rows.Scan(&r.Number, &r.Name, &r.Programmings, &r.PreProgrammings,
+			&r.Prevision, &r.CategoryName, &r.ChapterCode); err != nil {
 			return err
 		}
 		p.ProgrammingAndPrevisions = append(p.ProgrammingAndPrevisions, r)
