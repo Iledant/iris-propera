03f77ba29443793d111a47a72a8cea07455c874e
Iledant <christophe.saintillan@gmail.com>
2019-12-10 22:07:58 +0100

    Add step and category to detailed plan lines query

--- models/detailed_plan_line_prev.go
+++ models/detailed_plan_line_prev.go
@@ -38,37 +38,48 @@ func (d *DetailedPlanLineAndPrevisions) GetAll(plan *Plan, db *sql.DB) (err erro
 
 	finalQry := `SELECT json_build_object('id', q.id, 'name', q.name, 
 	'op_name', q.op_name, 'value', q.value, 'total_value', q.total_value,
-	'op_number',q.op_number, 'commitment_name', q.commitment_name, 
+	'op_number',q.op_number,'step',q.step,'category',q.category, 'commitment_name', q.commitment_name, 
 	'commitment_code', q.commitment_code, 'commitment_date', q.commitment_date, 
 	'commitment_value', q.commitment_value, 'programmings_value', q.programmings_value, 
 	'programmings_date', q.programmings_date,	` + jsonQry + `) FROM
-	(SELECT pl.id, pl.name, pl.total_value, pl.value, fc.op_number, fc.op_name,
+	(SELECT pl.id, pl.name, pl.total_value, pl.value, fc.op_number, fc.op_name,fc.Step,fc.category,
 		fc.commitment_name, fc.commitment_code, fc.commitment_date, fc.commitment_value,   
 		fc.programmings_value, fc.programmings_date, ` + prevQry + ` FROM plan_line pl
 	LEFT OUTER JOIN 
-	(SELECT op.number AS op_number, op.name as op_name, f.name AS commitment_name, 
+	(SELECT op.number AS op_number, op.name as op_name, step.name AS step,
+		category.name as category, f.name AS commitment_name, 
 		f.iris_code AS commitment_code, f.date AS commitment_date, f.value AS commitment_value, 
 		NULL AS programmings_value,NULL AS programmings_date, ` + nullQry + `, f.plan_line_id 
-	FROM financial_commitment f, physical_op op 
+		FROM financial_commitment f
+		JOIN physical_op op ON f.physical_op_id = op.id 
+		LEFT OUTER JOIN step ON op.step_id=step.id
+		LEFT OUTER JOIN category ON op.category_id=category.id
 	WHERE EXTRACT(year FROM f.date) < ` + actualYear + ` AND f.plan_line_id NOTNULL 
-		AND f.physical_op_id = op.id
 	UNION ALL
-	SELECT op.number AS op_number, op.name as op_name, NULL AS commitment_name, 
+	SELECT op.number AS op_number, op.name as op_name,step.name AS step,
+	category.name as category,  NULL AS commitment_name, 
 		NULL AS commitment_code, NULL AS commitment_date, NULL AS commitment_value, 
 		p.value AS programmings_value, c.date AS programmings_date, ` + nullQry + `, 
 		op.plan_line_id
-	FROM programmings p, physical_op op,commissions c 
-	WHERE p.year=` + actualYear + ` AND c.id=p.commission_id AND op.id=p.physical_op_id
+	FROM programmings p
+	JOIN commissions c ON c.id=p.commission_id
+	JOIN physical_op op ON p.physical_op_id = op.id 
+	LEFT OUTER JOIN step ON op.step_id=step.id
+	LEFT OUTER JOIN category ON op.category_id=category.id
+	WHERE p.year=` + actualYear + ` 
 	UNION ALL
-	SELECT op.number AS op_number, op.name as op_name, NULL AS commitment_name, 
+	SELECT op.number AS op_number, op.name as op_name, step.name AS step,
+	category.name as category, NULL AS commitment_name, 
 		NULL AS commitment_code, NULL AS commitment_date, NULL AS commitment_value, 
 		NULL AS programmings_value, NULL AS programmings_date, ` + colQry + `, op.plan_line_id
 	FROM crosstab (
 		'SELECT physical_op_id, year, value FROM prev_commitment ORDER BY 1,2',
 		'SELECT m FROM generate_series(` + strconv.FormatInt(firstYear, 10) + `, ` +
 		strconv.FormatInt(lastYear, 10) + `) AS m')
-		AS (physical_op_id INTEGER, ` + convertQry + `) , physical_op op
-	WHERE physical_op_id = op.id 
+		AS (physical_op_id INTEGER, ` + convertQry + `) 
+	JOIN physical_op op ON physical_op_id = op.id 
+	LEFT OUTER JOIN step ON op.step_id=step.id
+	LEFT OUTER JOIN category ON op.category_id=category.id
 	) fc ON fc.plan_line_id = pl.id
 	WHERE pl.plan_id = ` + strconv.FormatInt(plan.ID, 10) + `
 	ORDER BY 1,5,9,12) q`
