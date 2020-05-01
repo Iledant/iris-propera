675a3b62029b51c5d1825e203e40d44c15353d79
Iledant <christophe.saintillan@gmail.com>
2020-05-01 18:42:20 +0200

    Add csf and dc to payment demands stock

--- models/payment_demands_stocks.go
+++ models/payment_demands_stocks.go
@@ -8,8 +8,9 @@ import (
 
 // PaymentDemandsStock model
 type PaymentDemandsStock struct {
-	Day    time.Time `json:"day"`
-	Number int64     `json:"number"`
+	Day time.Time `json:"day"`
+	DC  int64     `json:"dc"`
+	CSF int64     `json:"csf"`
 }
 
 // PaymentDemandsStocks embeddes an array of PaymentDemandsStock for json export
@@ -20,18 +21,27 @@ type PaymentDemandsStocks struct {
 
 // GetAll fetches all payment demands count for every last 30 days
 func (p *PaymentDemandsStocks) GetAll(db *sql.DB) error {
-	rows, err := db.Query(`SELECT d.d,count(1)
-  FROM payment_demands p,(SELECT CURRENT_DATE-generate_series(0,30) d) d
-  WHERE p.receipt_date<=d.d
-    AND (p.processed_date ISNULL OR p.processed_date>d)
-    AND p.excluded<>FALSE
- GROUP BY 1 ORDER BY 1;`)
+	rows, err := db.Query(`select q1.d,q1.dc, q2.csf
+	FROM
+	(SELECT d.d,count(p) dc
+		FROM payment_demands p,(SELECT CURRENT_DATE-generate_series(0,30) d) d
+		WHERE p.receipt_date<=d.d
+			AND (p.processed_date ISNULL OR p.processed_date>d)
+			AND p.excluded<>FALSE GROUP BY 1) q1
+	JOIN
+	(SELECT d.d,count(p) csf
+		FROM payment_demands p,(SELECT CURRENT_DATE-generate_series(0,30) d) d
+		WHERE p.receipt_date<=d.d
+			AND (p.csf_date ISNULL OR p.csf_date>d)
+			AND p.excluded<>FALSE GROUP BY 1) q2
+		ON q1.d=q2.d
+	 ORDER BY 1;`)
 	if err != nil {
 		return fmt.Errorf("select %v", err)
 	}
 	var line PaymentDemandsStock
 	for rows.Next() {
-		if err = rows.Scan(&line.Day, &line.Number); err != nil {
+		if err = rows.Scan(&line.Day, &line.DC, &line.CSF); err != nil {
 			return fmt.Errorf("scan %v", err)
 		}
 		p.Lines = append(p.Lines, line)
